03c2597309a223b617267252b3676f75040dc44f
Short Course 010 <sc010>
2018-01-11 10:26:34 +0000

    Made it so that it takes three laps to win

--- Racer/Engine/src/Stages/maingame_frontend.js
+++ Racer/Engine/src/Stages/maingame_frontend.js
@@ -6,9 +6,9 @@
 
 
 OverDrive.Stages.MainGame = (function(stage, canvas, context) {
-  
+
   stage.MainGame.prototype.setup = function() {
-    
+
     this.createTrack();
     this.createScenery();
     this.setupBackground();
@@ -16,31 +16,33 @@ OverDrive.Stages.MainGame = (function(stage, canvas, context) {
     this.createPlayer1();
     this.createPlayer2();
     this.startClock();
-    
+
     this.startGameLoop();
   }
-  
-  
+
+
   stage.MainGame.prototype.mainLoop = function() {
-    
+
     this.updateClock();
-    
+
     this.animatePlayers();
-    
+
     this.drawNewAnimationFrame();
-    
+
+    // win conditions
+    // need to make dynamic?
     if (this.player1CrossedFinishLine() || this.player2CrossedFinishLine()) {
-      
+
       this.leaveGameLoop();
     }
     else {
-      
+
       this.repeatGameLoop();
     }
   }
-  
-  
-  
+
+
+
   return stage;
-  
+
 })((OverDrive.Stages.MainGame || {}), OverDrive.canvas, OverDrive.context);
